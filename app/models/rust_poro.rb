3204208e4137cadee085afda031b83f99f9ea7c5
Matt Pindell <pindell.m@gmail.com>
2016-05-21 15:56:04 -0600

    Add if/else to RustPoro

--- app/models/rust_poro.rb
+++ app/models/rust_poro.rb
@@ -2,13 +2,26 @@ require 'ffi'
 
 class RustPoro
   extend FFI::Library
-  ffi_lib_path = Rails.root.join(
-    'embedded',
-    'rust',
-    'target',
-    'release',
-    'libnth_prime.dylib'
-  )
+
+  if Rails.env == "development"
+    ffi_lib_path = Rails.root.join(
+      'app',
+      'embedded',
+      'rust',
+      'target',
+      'release',
+      'libnth_prime.dylib'
+    )
+  else
+    ffi_lib_path = Rails.root.join(
+      'embedded',
+      'rust',
+      'target',
+      'release',
+      'libnth_prime.dylib'
+    )
+  end
+
   ffi_lib ffi_lib_path
   attach_function :nth_prime, [:int], :int
 end
